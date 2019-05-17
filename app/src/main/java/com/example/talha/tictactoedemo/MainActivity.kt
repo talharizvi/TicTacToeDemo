1d85ba97363c8045d03aaf9ab5be29a18ed07d34
talharizvi <talharizvi480@gmail.com>
2019-05-18 00:31:29 +0530

    removed dead code

--- app/src/main/java/com/example/talha/tictactoedemo/MainActivity.kt
+++ app/src/main/java/com/example/talha/tictactoedemo/MainActivity.kt
@@ -13,22 +13,9 @@ import kotlin.collections.ArrayList
 
 class MainActivity:AppCompatActivity() {
 
-    lateinit var testBtn:Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-
-//        var btn1 = findViewById<ImageButton>(R.id.btn_1)
-//        var btn2 = findViewById<ImageButton>(R.id.btn_2)
-//        var btn3 = findViewById<ImageButton>(R.id.btn_3)
-//        var btn4 = findViewById<ImageButton>(R.id.btn_4)
-//        var btn5 = findViewById<ImageButton>(R.id.btn_5)
-//        var btn6 = findViewById<ImageButton>(R.id.btn_6)
-//        var btn7 = findViewById<ImageButton>(R.id.btn_7)
-//        var btn8 = findViewById<ImageButton>(R.id.btn_8)
-//        var btn9 = findViewById<ImageButton>(R.id.btn_9)
-
     }
 
     protected fun clickMethod(view:View){
@@ -38,11 +25,9 @@ class MainActivity:AppCompatActivity() {
             when(btnSelected.id){
                 R.id.btn1 -> {
                     cellId=1
-                //    btnSelected.setText("X")
                 }
                 R.id.btn2 -> {
                     cellId=2
-                  //  btnSelected.setText("O")
                 }
                 R.id.btn3 -> cellId=3
                 R.id.btn4 -> cellId=4
@@ -52,15 +37,8 @@ class MainActivity:AppCompatActivity() {
                 R.id.btn8 -> cellId=8
                 R.id.btn9 -> cellId=9
             }
-//            if (cellId%2==0){
-//                btnSelected.setText("0")
-//            }else{
-//                btnSelected.setText("X")
-//            }
-
-          //  Toast.makeText(this@MainActivity,"Id:"+cellId,Toast.LENGTH_SHORT).show()
         }catch (e:Exception){
-           // Toast.makeText(this@MainActivity,e.toString(),Toast.LENGTH_SHORT).show()
+
         }
 
         playGame(cellId,btnSelected)
@@ -90,48 +68,34 @@ class MainActivity:AppCompatActivity() {
 
        if (player1.contains(1)&&player1.contains(2)&&player1.contains(3)){
            winner = 1
-           //Toast.makeText(this,"Player1 wins",Toast.LENGTH_SHORT).show()
        }else if (player1.contains(4)&&player1.contains(5)&&player1.contains(6)){
            winner = 1
-           //Toast.makeText(this,"Player1 wins",Toast.LENGTH_SHORT).show()
        }else if (player1.contains(7)&&player1.contains(8)&&player1.contains(9)){
            winner = 1
-           //Toast.makeText(this,"Player1 wins",Toast.LENGTH_SHORT).show()
        }else if (player1.contains(1)&&player1.contains(4)&&player1.contains(7)){
            winner = 1
-           //Toast.makeText(this,"Player1 wins",Toast.LENGTH_SHORT).show()
        }else if (player1.contains(2)&&player1.contains(5)&&player1.contains(8)){
            winner = 1
-           //Toast.makeText(this,"Player1 wins",Toast.LENGTH_SHORT).show()
        }else if (player1.contains(3)&&player1.contains(6)&&player1.contains(9)){
            winner = 1
-           //Toast.makeText(this,"Player1 wins",Toast.LENGTH_SHORT).show()
        }else if (player1.contains(1)&&player1.contains(5)&&player1.contains(9)){
            winner = 1
-           //Toast.makeText(this,"Player1 wins",Toast.LENGTH_SHORT).show()
        }
 
         if (player2.contains(1)&&player2.contains(2)&&player2.contains(3)){
             winner = 2
-            //Toast.makeText(this,"Player2 wins",Toast.LENGTH_SHORT).show()
         }else if (player2.contains(4)&&player2.contains(5)&&player2.contains(6)){
             winner = 2
-            //Toast.makeText(this,"Player2 wins",Toast.LENGTH_SHORT).show()
         }else if (player2.contains(7)&&player2.contains(8)&&player2.contains(9)){
             winner = 2
-            //Toast.makeText(this,"Player2 wins",Toast.LENGTH_SHORT).show()
         }else if (player2.contains(1)&&player2.contains(4)&&player2.contains(7)){
             winner = 2
-            //Toast.makeText(this,"Player2 wins",Toast.LENGTH_SHORT).show()
         }else if (player2.contains(2)&&player2.contains(5)&&player2.contains(8)){
             winner = 2
-            //Toast.makeText(this,"Player2 wins",Toast.LENGTH_SHORT).show()
         }else if (player2.contains(3)&&player2.contains(6)&&player2.contains(9)){
             winner = 2
-            //Toast.makeText(this,"Player2 wins",Toast.LENGTH_SHORT).show()
         }else if (player2.contains(1)&&player2.contains(5)&&player2.contains(9)){
             winner = 2
-            //Toast.makeText(this,"Player2 wins",Toast.LENGTH_SHORT).show()
         }
 
         if (winner!=-1){
@@ -141,8 +105,6 @@ class MainActivity:AppCompatActivity() {
                 Toast.makeText(this,"Player2 wins",Toast.LENGTH_SHORT).show()
             }
         }
-
-
     }
 
     fun autoPlay(){
